112e3c23b6d4ece20e322e132ea3af099fcc7f16
Nguyễn Thị Hồng Thơ <22151305@student.hcmute.edu.vn>
2024-10-24 22:00:19 +0700

    add

---
+++ DoAnNhom21/Connection.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace DoAnNhom21
+{
+    internal class Connection
+    {
+        public static string user;
+        public static string pass;
+
+        public static string connectString = Properties.Settings.Default.ConnectionString;
+        public static string GetConnectionString(string user, string pass)
+        {
+            return connectString + ";User Id=" + user + ";Password=" + pass;
+        }
+
+        public static SqlConnection getConnection()
+        {
+            return new SqlConnection(connectString);
+        }
+
+        public static DataTable LoadDataTable(string query)
+        {
+            connectString = GetConnectionString(user, pass);
+            SqlConnection conn = new SqlConnection(connectString);
+            DataTable dataTable = new DataTable();
+            try
+            {
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                adapter.Fill(dataTable);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dataTable;
+        }
+
+        public static DataTable LoadDataTable(SqlCommand command)
+        {
+            connectString = GetConnectionString(user, pass);
+            SqlConnection conn = new SqlConnection(connectString);
+            DataTable dataTable = new DataTable();
+            try
+            {
+                command.Connection = conn;
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(dataTable);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dataTable;
+        }
+
+        public static void ExecuteCommand(SqlCommand command)
+        {
+            connectString = GetConnectionString(user, pass);
+            using (SqlConnection connection = new SqlConnection(connectString))
+            {
+                command.Connection = connection;
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+
+        }
+        public static void ExecuteCommandAdmin(SqlCommand command)
+        {
+            using (SqlConnection connection = new SqlConnection(connectString))
+            {
+                command.Connection = connection;
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ DoAnNhom21/FormThongTinHopDong.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace DoAnNhom21
+{
+    public partial class FormThongTinHopDong : Form
+    {
+        SqlConnection conn;
+        public FormThongTinHopDong()
+        {
+            InitializeComponent();
+        }
+
+        private void FormThongTinHopDong_Load(object sender, EventArgs e)
+        {
+            conn = Connection.getConnection();
+            conn.Open();
+            string data = "Select * from View_HopDongNhanVien";
+            dataGridViewThongTinHopDong.DataSource = Connection.LoadDataTable(data);
+        }
+
+        private void btnXuatExcel_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
