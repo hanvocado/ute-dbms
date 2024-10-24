03b946f00254893b135f4208f8222cd2daf01991
ntnhtc <han.ntnhtc@gmail.com>
2024-10-24 22:01:21 +0700

    update logic TinhLuong

--- DoAnNhom21/FormThongKeLuong.cs
+++ DoAnNhom21/FormThongKeLuong.cs
@@ -13,8 +13,6 @@ namespace DoAnNhom21
         }
         void load()
         {
-            string data1 = "Select * from v_QuanLyLuong";
-            dgvLuong.DataSource = Connection.LoadDataTable(data1);
             string queryAllThang = "SELECT MaThang, MoTa FROM Thang ORDER By MaThang DESC";
             DataTable dtThang = Connection.LoadDataTable(queryAllThang);
             cbbMaThang.Items.Clear();
@@ -30,17 +28,10 @@ namespace DoAnNhom21
 
         private void btnXemLuong_Click(object sender, EventArgs e)
         {
-            try
-            {
-                SqlCommand cmd = new SqlCommand("SELECT * FROM v_QuanLyLuong WHERE MaThang = @MaThang");
-                cmd.Parameters.AddWithValue("@MaThang", this.cbbMaThang.SelectedValue.ToString());
-
-                dgvLuong.DataSource = Connection.LoadDataTable(cmd);
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show("Lỗi khi lọc dữ liệu: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            SqlCommand cmd = new SqlCommand("sp_TinhLuongTheoThang");
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@MaThang", cbbMaThang.SelectedValue);
+            dgvLuong.DataSource = Connection.LoadDataTable(cmd);
         }
     }
 }
